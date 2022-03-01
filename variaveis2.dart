c5e4f158ecd040a78faf8fdb718bf9b29be0ed24
Gabriel S. Souza <gabriel12saraiva@gmail.com>
2022-03-01 17:09:58 -0300

    Added variaveis2 file

---
+++ variaveis2.dart
@@ -0,0 +1,12 @@
+void main() {
+  //const:
+  const constNome = "Dart const";
+
+  //final:
+  final finalNome = "Dart final";
+
+  final finalNomeComConst = const ["Dart final"];
+
+  print(constNome);
+  print(finalNome);
+}
