85aac143da571d59639558295fceaae121e88d9a
Gabriel S. Souza <gabriel12saraiva@gmail.com>
2022-03-06 14:57:43 -0300

    Correct comments

--- funcoes-parametros-nomeados.dart
+++ funcoes-parametros-nomeados.dart
@@ -1,7 +1,7 @@
 void main() {
 
   // Definindo função com parâmetros opicionais --> apresentam valores padrão
-  // Os parâmetros list e gifts apresentam valores padrão:
+  // Os parâmetros list e gifts caso não sejam inseridos na chamada, mostrarão valores padrão:
   void doStuff(
       {List<int> list = const [1, 2, 3],
       Map<String, String> gifts = const {
