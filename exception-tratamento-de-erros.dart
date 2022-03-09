5bbfaeca03c66f30194a1b5d344c19479a8a016f
Gabriel S. Souza <gabriel12saraiva@gmail.com>
2022-03-09 12:00:12 -0300

    Add exception-tratamento-de-erros file

---
+++ exception-tratamento-de-erros.dart
@@ -0,0 +1,50 @@
+void main(args) {
+  try {
+    gerarErro(int.parse(args[0]));
+  } 
+  //Filtrando as excessões para criar tratamentos específicos
+  on Valor1Exception {
+    print("Tratando a excessão: Valor 1");
+  } 
+  on Valor2Exception {
+    print("Tratando a excessão: Valor 2");
+  } 
+  on Valor3Exception {
+    print("Tratando a excessão: Valor 3");
+  } 
+  on Exception catch (e) {
+    print("Não foi possível: $e");
+  } catch (e) {
+    print("Houve um erro: $e");
+  }
+}
+
+void gerarErro(int numero) {
+  //lança apenas uma, como o return
+  if (numero == 1) {
+    throw Valor1Exception();
+  }
+  if (numero == 2) {
+    throw Valor2Exception();
+  }
+  if (numero == 3) {
+    throw Valor3Exception();
+  }
+  if (numero == 4) {
+    throw Error();
+  }
+
+  throw Exception();
+}
+
+// Cariando classes de exceptions específicas para a regra de negócio
+// Essas classes implementam a classe Exception
+class Valor1Exception implements Exception {}
+
+class Valor2Exception implements Exception {}
+
+class Valor3Exception implements Exception {}
+
+
+//entrada: dart exception-tratamento-de-erros.dart 1
+//saída: Tratando a excessão: Valor 1
