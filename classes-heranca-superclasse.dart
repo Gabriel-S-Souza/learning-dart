6bebaa73d890736c35d4f001cf304c729f82254e
Gabriel S. Souza <gabriel12saraiva@gmail.com>
2022-03-09 16:53:28 -0300

    Add classes-heranca-superclasse file

---
+++ classes-heranca-superclasse.dart
@@ -0,0 +1,30 @@
+//Cria uma superclasse
+class ClienteModel {
+  late String nome;
+  ClienteModel.fromJson(Map data) {
+    // pegando o valor contido na chave "nome"
+    this.nome = data["nome"];
+  }
+  String diz(String mensagem) {
+    return "${this.nome} diz $mensagem";
+  }
+}
+
+//Herda (extendes) a ClienteModel
+class Cliente extends ClienteModel {
+  late String nome;
+
+  //Cria um construtor que utiliza um construtor nomeado da superclasse
+  // O super permite acessar as propriedades e métodos da superclasse
+  Cliente(Map data) : super.fromJson(data);
+}
+
+void main() {
+  final cliente = Cliente({"nome": "Fulano"});
+  print(cliente.nome);
+  print(cliente.diz("Olá mundo"));
+}
+
+// saída:
+// Fulano
+// Fulano diz Olá mundo
