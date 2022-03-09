dbfb0232008d26168be78af4b09bd9ceddc9b6f5
Gabriel S. Souza <gabriel12saraiva@gmail.com>
2022-03-09 13:03:55 -0300

    Add classes file

---
+++ classes.dart
@@ -0,0 +1,20 @@
+void main() {
+  //As classes também são tipos específicos
+  Funcionario funcionario = Funcionario();
+  print(funcionario.salrio);
+  print(funcionario.registrarPonto());
+}
+
+class Funcionario {
+  //Propriedades
+  String nome = "Fulano";
+  int idade = 23;
+  double salrio = 4000.00;
+  bool contaAtiva = true;
+
+  //Métodos
+  DateTime registrarPonto() {
+    //Retorna uma instância [DateTime] com data e hora atuais no fuso horário local
+    return DateTime.now();
+  }
+}
